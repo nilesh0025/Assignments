b45bc7da53ef5a022d2aedbba208f24e2ad342cc
nilesh0025 <104484813+nilesh0025@users.noreply.github.com>
2022-11-17 17:24:36 +0530

    Add files via upload

---
+++ exceptionhandling/exceptionhandling/assignment5/TrainTicket.java
@@ -0,0 +1,14 @@
+package com.exceptionhandling.assignment5;
+
+import java.time.LocalTime;
+
+public class TrainTicket {public static void main(String[] args) {
+	
+
+	 LocalTime time1 = LocalTime.now(); 
+	 System.out.println(time1);
+	 for(int time=10;time<=12;time++) {
+		 
+	 }
+}
+}
